983343b039a59c0ab13dcd80834002c53abc95f8
Tomas Jansson <tomas.jansson@gmail.com>
2015-06-23 23:40:25 +0200

    getting started done

--- build.fsx
+++ build.fsx
@@ -2,8 +2,10 @@
 #r @"packages/FAKE/tools/FakeLib.dll"
 open Fake
 open Fake.Testing
+open Fake.NuGetHelper
 
 let buildDir = "./.build/"
+let packagingDir = buildDir + "_PublishedWebsites/FAKESimple.Web"
 let deployDir = "./.deploy/"
 let testDir = "./.test/"
 
@@ -53,6 +55,19 @@ Target "Web" (fun _ ->
 
 Target "Package" (fun _ ->
   trace "Packing the web"
+  let versionCandidate = (environVar "version")
+  let version = if versionCandidate = "" || versionCandidate = null then "0.0.0" else versionCandidate
+  NuGet (fun p ->
+        {p with
+            Authors = ["Tomas Jansson"]
+            Project = "FAKESimple.Demo"
+            Description = "Demoing FAKE"
+            OutputPath = deployDir
+            Summary = "Does this work"
+            WorkingDir = packagingDir
+            Version = version
+            Publish = false })
+            (packagingDir + "/FAKESimple.Web.nuspec")
 )
 
 Target "Default" (fun _ ->
@@ -64,8 +79,20 @@ Target "Default" (fun _ ->
 ==> "BuildTest"
 ==> "Test"
 ==> "Default"
-==> "Web"
 ==> "Package"
+==> "Web"
+
+Target "A" (fun _ -> trace "This is A")
+Target "B" (fun _ -> trace "This is B")
+Target "C" (fun _ -> trace "This is C")
+Target "D" (fun _ -> trace "This is D")
+Target "E" (fun _ -> trace "This is E")
+
+"A"
+  ==> "B"
+    ==> "C"
+  ==> "D"
+    ==> "E"
 
 // start build
 RunTargetOrDefault "Default"
