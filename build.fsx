944ebfd3a7adb65077e6269124989e8921fd6f53
Tomas Jansson <tomas.jansson@gmail.com>
2015-10-25 22:36:23 +0100

    structured the code somewhat #wat

--- build.fsx
+++ build.fsx
@@ -7,6 +7,7 @@ open Fake.NuGetHelper
 open Fake.OctoTools
 open System.IO
 
+[<AutoOpen>]
 module Npm =
   open System
 
@@ -62,154 +63,151 @@ module Npm =
   let Npm f =
     npmParams |> f |> run
 
-open Npm
+[<AutoOpen>]
+module OctoHelpers =
+  let executeOcto command =
+    let serverName = environVar "OCTO_SERVER"
+    let apiKey = environVar "OCTO_KEY"
+    let server = { Server = serverName; ApiKey = apiKey }
+    Octo (fun octoParams ->
+        { octoParams with
+            ToolPath = "./packages/octopustools"
+            Server   = server
+            Command  = command }
+    )
 
-let buildDir = "./.build/"
-let packagingDir = buildDir + "FAKESimple.Web/_PublishedWebsites/FAKESimple.Web"
-let deployDir = "./.deploy/"
-let testDir = "./.test/"
-let projects = !! "src/**/*.csproj" -- "src/**/*.Tests.csproj"
-let testProjects = !! "src/**/*.Tests.csproj"
-let packages = !! "./**/packages.config"
+[<AutoOpen>]
+module AppVeyorHelpers =
+  let execOnAppveyor arguments =
+    let result =
+      ExecProcess (fun info ->
+        info.FileName <- "appveyor"
+        info.Arguments <- arguments
+        ) (System.TimeSpan.FromMinutes 2.0)
+    if result <> 0 then failwith (sprintf "Failed to execute appveyor command: %s" arguments)
+    trace "Published packages"
 
-let getOutputDir proj =
-  let folderName = Directory.GetParent(proj).Name
-  sprintf "%s%s/" buildDir folderName
+  let publishOnAppveyor folder =
+    !! (folder + "*.nupkg")
+    |> Seq.iter (fun artifact -> execOnAppveyor (sprintf "PushArtifact %s" artifact))
 
-let build proj =
-  let outputDir = proj |> getOutputDir
-  MSBuildRelease outputDir "ResolveReferences;Build" [proj] |> ignore
+[<AutoOpen>]
+module Helpers =
+  let buildDir = "./.build/"
+  let packagingDir = buildDir + "FAKESimple.Web/_PublishedWebsites/FAKESimple.Web"
+  let deployDir = "./.deploy/"
+  let testDir = "./.test/"
+  let projects = !! "src/**/*.csproj" -- "src/**/*.Tests.csproj"
+  let testProjects = !! "src/**/*.Tests.csproj"
+  let packages = !! "./**/packages.config"
 
-Target "Clean" (fun() ->
-  trace "Cleaing your world!"
-  CleanDirs [buildDir; deployDir; testDir]
-)
+  let getOutputDir proj =
+    let folderName = Directory.GetParent(proj).Name
+    sprintf "%s%s/" buildDir folderName
 
-Target "RestorePackages" (fun _ ->
-  packages
-  |> Seq.iter (RestorePackage (fun p -> {p with OutputPath = "./src/packages"}))
-)
+  let build proj =
+    let outputDir = proj |> getOutputDir
+    MSBuildRelease outputDir "ResolveReferences;Build" [proj] |> ignore
 
-Target "Build" (fun() ->
-  trace "Building again!"
+  let getVersion() =
+    let buildCandidate = (environVar "APPVEYOR_BUILD_NUMBER")
+    if buildCandidate = "" || buildCandidate = null then "1.0.0" else (sprintf "1.0.0.%s" buildCandidate)
 
-  projects
-  |> Seq.iter build
-)
-
-Target "BuildTest" (fun() ->
-  trace "Building the tests again!"
-  testProjects
-  |> MSBuildDebug testDir "Build"
-  |> ignore
-)
-
-Target "Test" (fun() ->
-  trace "Testing your stuff!"
-  !! (testDir + "/*.Tests.dll")
-      |> xUnit2 (fun p ->
-          {p with
-              ShadowCopy = false;
-              HtmlOutputPath = Some (testDir @@ "xunit.html");
-              XmlOutputPath = Some (testDir @@ "xunit.xml");
-          })
-)
-
-// Default target
-Target "Web" (fun _ ->
-  Npm (fun p ->
-    { p with
-        Command = Install Standard
-        WorkingDirectory = "./src/FAKESimple.Web/"
-    })
-
-  Npm (fun p ->
-    { p with
-        Command = (Run "build")
-        WorkingDirectory = "./src/FAKESimple.Web/"
-    })
-
-  trace "Hello World from FAKE"
-)
-
-Target "CopyWeb" (fun _ ->
-  let targetDir = packagingDir @@ "dist"
-  let sourceDir = "./src/FAKESimple.Web/dist"
-  CopyDir targetDir sourceDir (fun x -> true)
-)
-
-let getVersion() =
-  let buildCandidate = (environVar "APPVEYOR_BUILD_NUMBER")
-  if buildCandidate = "" || buildCandidate = null then "1.0.0" else (sprintf "1.0.0.%s" buildCandidate)
-
-Target "Package" (fun _ ->
-  trace "Packing the web"
-  let version = getVersion()
-  NuGet (fun p ->
-        {p with
-            Authors = ["Tomas Jansson"]
-            Project = "FAKESimple.Web"
-            Description = "Demoing FAKE"
-            OutputPath = deployDir
-            Summary = "Does this work"
-            WorkingDir = packagingDir
-            Version = version
-            Publish = false })
-            (packagingDir + "/FAKESimple.Web.nuspec")
-)
-
-let execOnAppveyor arguments =
-  let result =
-    ExecProcess (fun info ->
-      info.FileName <- "appveyor"
-      info.Arguments <- arguments
-      ) (System.TimeSpan.FromMinutes 2.0)
-  if result <> 0 then failwith (sprintf "Failed to execute appveyor command: %s" arguments)
-  trace "Published packages"
-
-let publishOnAppveyor folder =
-  !! (folder + "*.nupkg")
-  |> Seq.iter (fun artifact -> execOnAppveyor (sprintf "PushArtifact %s" artifact))
-
-Target "Publish" (fun _ ->
-  match buildServer with
-  | BuildServer.AppVeyor ->
-      publishOnAppveyor deployDir
-  | _ -> ()
-)
-
-let executeOcto command =
-  let serverName = environVar "OCTO_SERVER"
-  let apiKey = environVar "OCTO_KEY"
-  let server = { Server = serverName; ApiKey = apiKey }
-  Octo (fun octoParams ->
-      { octoParams with
-          ToolPath = "./packages/octopustools"
-          Server   = server
-          Command  = command }
+[<AutoOpen>]
+module Targets =
+  Target "Clean" (fun() ->
+    CleanDirs [buildDir; deployDir; testDir]
   )
 
-Target "Create release" (fun _ ->
-  let version = getVersion()
-  let release = CreateRelease({ releaseOptions with Project = "FAKESimple.Web"; Version = version }, None)
-  executeOcto release
-)
+  Target "RestorePackages" (fun _ ->
+    packages
+    |> Seq.iter (RestorePackage (fun p -> {p with OutputPath = "./src/packages"}))
+  )
 
-Target "Deploy" (fun _ ->
-  let version = getVersion()
-  let deploy = DeployRelease(
-                { deployOptions with
-                    Project = "FAKESimple.Web"
-                    Version = version
-                    DeployTo = "Prod"
-                    WaitForDeployment = true})
-  executeOcto deploy
-)
+  Target "Build" (fun() ->
+    projects
+    |> Seq.iter build
+  )
 
-Target "Default" (fun _ ->
-  trace "Done"
-  ()
-)
+  Target "Web" (fun _ ->
+    Npm (fun p ->
+      { p with
+          Command = Install Standard
+          WorkingDirectory = "./src/FAKESimple.Web/"
+      })
+
+    Npm (fun p ->
+      { p with
+          Command = (Run "build")
+          WorkingDirectory = "./src/FAKESimple.Web/"
+      })
+  )
+
+  Target "CopyWeb" (fun _ ->
+    let targetDir = packagingDir @@ "dist"
+    let sourceDir = "./src/FAKESimple.Web/dist"
+    CopyDir targetDir sourceDir (fun x -> true)
+  )
+
+  Target "BuildTest" (fun() ->
+    testProjects
+    |> MSBuildDebug testDir "Build"
+    |> ignore
+  )
+
+  Target "Test" (fun() ->
+    !! (testDir + "/*.Tests.dll")
+        |> xUnit2 (fun p ->
+            {p with
+                ShadowCopy = false;
+                HtmlOutputPath = Some (testDir @@ "xunit.html");
+                XmlOutputPath = Some (testDir @@ "xunit.xml");
+            })
+  )
+
+  Target "Package" (fun _ ->
+    trace "Packing the web"
+    let version = getVersion()
+    NuGet (fun p ->
+          {p with
+              Authors = ["Tomas Jansson"]
+              Project = "FAKESimple.Web"
+              Description = "Demoing FAKE"
+              OutputPath = deployDir
+              Summary = "Does this work"
+              WorkingDir = packagingDir
+              Version = version
+              Publish = false })
+              (packagingDir + "/FAKESimple.Web.nuspec")
+  )
+
+  Target "Publish" (fun _ ->
+    match buildServer with
+    | BuildServer.AppVeyor ->
+        publishOnAppveyor deployDir
+    | _ -> ()
+  )
+
+  Target "Create release" (fun _ ->
+    let version = getVersion()
+    let release = CreateRelease({ releaseOptions with Project = "FAKESimple.Web"; Version = version }, None)
+    executeOcto release
+  )
+
+  Target "Deploy" (fun _ ->
+    let version = getVersion()
+    let deploy = DeployRelease(
+                  { deployOptions with
+                      Project = "FAKESimple.Web"
+                      Version = version
+                      DeployTo = "Prod"
+                      WaitForDeployment = true})
+    executeOcto deploy
+  )
+
+  Target "Default" (fun _ ->
+    ()
+  )
 
 "Clean"
 ==> "RestorePackages"
@@ -224,11 +222,4 @@ Target "Default" (fun _ ->
 ==> "Deploy"
 ==> "Default"
 
-Target "TryOcto" (fun _ ->
-  let deploy = DeployRelease({deployOptions with Project = "FAKESimple.Web"; Version = "1.0.0.23"})
-  executeOcto deploy
-)
-
-// start build
 RunTargetOrDefault "Build"
-//New-AzurePublicIpAddress -Name connecttointernet -ResourceGroupName FakeAppveyorDemo -DomainNameLabel "fakeocto" -Location "North Europe" -AllocationMethod Dynamic
